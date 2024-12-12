09ac01c97681c00e1426d82aa44fbc178a6bfc5c
unknown <ali.riahi.jfy.3@gmail.com>
2024-12-12 23:46:25 +0330

    Your commit message

--- accounts/views.py
+++ accounts/views.py
@@ -4,16 +4,19 @@ from .forms import RegistrationForm, LoginForm
 from django.contrib import messages
 
 def auth_page(request):
+    reg_form = RegistrationForm()
+    login_form = LoginForm()
+
     if request.method == "POST":
-        if "register" in request.POST:
+        if "register" in request.POST:  # فرم ثبت‌نام ارسال شده
             reg_form = RegistrationForm(request.POST)
-            login_form = LoginForm()
             if reg_form.is_valid():
                 reg_form.save()  # رمز عبور به صورت هش‌شده ذخیره می‌شود
-                messages.success(request, "ثبت‌نام با موفقیت انجام شد!")
-                return redirect('accounts:auth_page')
-        elif "login" in request.POST:
-            reg_form = RegistrationForm()
+                messages.success(request, "ثبت‌نام با موفقیت انجام شد! اکنون وارد شوید.")
+                return redirect('accounts:auth_page')  # بازگشت به صفحه ورود
+            else:
+                messages.error(request, "خطا در ثبت‌نام. لطفاً اطلاعات وارد شده را بررسی کنید.")
+        elif "login" in request.POST:  # فرم ورود ارسال شده
             login_form = LoginForm(data=request.POST)
             if login_form.is_valid():
                 user = authenticate(
@@ -23,13 +26,10 @@ def auth_page(request):
                 )
                 if user:
                     login(request, user)
-                    return redirect('homepage:homepage')
+                    return redirect('homepage:homepage')  # به صفحه اصلی هدایت شوید
                 else:
                     messages.error(request, "شماره موبایل یا رمز عبور نادرست است.")
             else:
                 messages.error(request, "فرم ورود معتبر نیست.")
-    else:
-        reg_form = RegistrationForm()
-        login_form = LoginForm()
 
     return render(request, 'accounts/auth_page.html', {'reg_form': reg_form, 'login_form': login_form})
