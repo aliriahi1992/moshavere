635428c7ffdb38dd3edaea0a7837dfafc714626d
unknown <ali.riahi.jfy.3@gmail.com>
2024-12-20 13:14:09 +0330

    Your commit message

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class RegistrationForm(UserCreationForm):
+    mobile_number = forms.CharField(label="شماره موبایل")
+    full_name = forms.CharField(label="نام کامل")
+    password1 = forms.CharField(label="رمز عبور", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="تأیید رمز عبور", widget=forms.PasswordInput)
+    
     class Meta:
         model = CustomUser
         fields = ('mobile_number', 'full_name', 'password1', 'password2')
@@ -16,6 +21,7 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="شماره موبایل")
