99788a69b1bb93c52510f653a0d7b5699818668c
unknown <ali.riahi.jfy.3@gmail.com>
2024-12-20 13:39:53 +0330

    Your commit message

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('auth/', auth_page, name='auth_page'),  # مسیر auth
     path('', auth_page, name='auth_page'),   # مسیر پیش‌فرض برای صفحه اصلی
+    path('logout/', LogoutView.as_view(next_page='/accounts/auth/'), name='logout'), # مسیر خروج از حساب کاربری
 ]
