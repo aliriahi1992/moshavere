667b00b5564f9bd95e826bb90fd1a6fbac8cd5bf
unknown <ali.riahi.jfy.3@gmail.com>
2024-12-20 12:50:31 +0330

    Your commit message

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView # اضافه کردن ایمپورت LogoutView
+from django.contrib.auth.views import LogoutView
 from .views import auth_page
 
 app_name = 'accounts'
@@ -7,5 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('auth/', auth_page, name='auth_page'),  # مسیر auth
     path('', auth_page, name='auth_page'),   # مسیر پیش‌فرض برای صفحه اصلی
-    path('logout/', LogoutView.as_view(next_page='/accounts/auth/'), name='logout'), # مسیر خروج از حساب کاربری
+    path('logout/', LogoutView.as_view(next_page='accounts:auth_page'), name='logout'),  # مسیر خروج از حساب کاربری با استفاده از نام فضای نامی
 ]
