9c52413f7be9ec2adbbd3c9a21e37f41fcf76448
unknown <ali.riahi.jfy.3@gmail.com>
2025-02-11 23:57:01 +0330

    Your commit message

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,17 +4,17 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('mobile_number', 'full_name', 'balance', 'is_staff', 'is_admin', 'is_active' ,'is_forget','is_webservice','webservice_from_who','webservice_origin')  # اضافه کردن balance به list_display
+    list_display = ('mobile_number', 'full_name', 'balance', 'is_staff', 'is_admin', 'is_active' ,'is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name')  # اضافه کردن balance به list_display
     list_filter = ('is_staff', 'is_admin', 'is_active','is_webservice')
     fieldsets = (
         (None, {'fields': ('mobile_number', 'password')}),
         ('اطلاعات شخصی', {'fields': ('full_name', 'balance')}),  # اضافه کردن balance به fieldsets
-        ('دسترسی‌ها', {'fields': ('is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin')}),
+        ('دسترسی‌ها', {'fields': ('is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('mobile_number', 'full_name', 'password1', 'password2', 'balance', 'is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin'),
+            'fields': ('mobile_number', 'full_name', 'password1', 'password2', 'balance', 'is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name'),
         }),
     )
     search_fields = ('mobile_number', 'full_name')
