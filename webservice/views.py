3498b3affcf108d96241310af7637f2d04e516d0
unknown <ali.riahi.jfy.3@gmail.com>
2025-02-11 23:32:55 +0330

    Your commit message

--- webservice/views.py
+++ webservice/views.py
@@ -109,7 +109,7 @@ def webservice_chat_view(request):
                         model = genai.GenerativeModel('gemini-pro')
 
                         if section == 1:
-                            prompt = "به عنوان یک مشاور در زمینه پوست و مو پاسخ این سوال رو بده و برای کاربر جذابیت ایجاد کن که از " + user.full_name + " خریدش رو به صورت اینترنتی انجام بده ولی خیلی هم شلوغش نکن و شعاری نباشه حرف هات : " + str(question)
+                            prompt = "به عنوان یک مشاور در زمینه پوست و مو پاسخ این سوال رو بده و در آخر هم به مخاطب پیشنعاد بده که خریدش رو از " + user.full_name + "  میتونه به راحتی انجام بده : " + str(question)
                         elif section == 2:
                             prompt = "به عنوان یک استاد علمی و دانشگاهی و معلم پاسخ این سوال رو بده  " + str(question)
                         elif section == 3:
