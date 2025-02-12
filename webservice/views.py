8d5d582f25a7c2a5d19de130006eb4b15159b53c
unknown <ali.riahi.jfy.3@gmail.com>
2025-02-13 01:28:19 +0330

    Your commit message

--- webservice/views.py
+++ webservice/views.py
@@ -148,7 +148,7 @@ def webservice_chat_view(request):
                         
                         campaign = " و بعد بهش " + user.webservice_campaign_name + " رو بهش پیشنهاد کن که بدون نیاز به کد تخفیف میتونه ازش استفاده کنه "
                         discount_code = " و بعد کد تخفیف " + user.webservice_discount_code + " رو برای بهره مندی از " + user.webservice_campaign_name + " پیشنهاد کن "
-                        name = " و در پایان هم در یک جمله ازش بابت مشاوره گرفتن از " + user.full_name + " صمیمانه تشکر کن ."
+                        name = " و در پایان هم بابت مشاوره گرفتن از " + user.full_name + " صمیمانه تشکر کن ."
                         question = " : " + str(question)
 
                         #در صورتیکه کد تخفیف وارد شده بود دیگر کمپین را جداگانه معرفی نکند
