2118571ca7ab16f678ddca26b46dca0d2db8b6e9
unknown <ali.riahi.jfy.3@gmail.com>
2025-02-14 10:21:25 +0330

    Your commit message

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,17 +4,17 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('mobile_number', 'full_name', 'balance', 'is_staff', 'is_admin', 'is_active' ,'is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name','webservice_total_request_limit','webservice_user_request_limit')  # اضافه کردن balance به list_display
+    list_display = ('mobile_number', 'full_name', 'balance', 'is_staff', 'is_admin', 'is_active' ,'is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name','webservice_total_request_limit','webservice_user_request_limit','webservice_question_price')  # اضافه کردن balance به list_display
     list_filter = ('is_staff', 'is_admin', 'is_active','is_webservice')
     fieldsets = (
         (None, {'fields': ('mobile_number', 'password')}),
         ('اطلاعات شخصی', {'fields': ('full_name', 'balance')}),  # اضافه کردن balance به fieldsets
-        ('دسترسی‌ها', {'fields': ('is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name','webservice_total_request_limit','webservice_user_request_limit')}),
+        ('دسترسی‌ها', {'fields': ('is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name','webservice_total_request_limit','webservice_user_request_limit','webservice_question_price')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('mobile_number', 'full_name', 'password1', 'password2', 'balance', 'is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name','webservice_total_request_limit','webservice_user_request_limit'),
+            'fields': ('mobile_number', 'full_name', 'password1', 'password2', 'balance', 'is_staff', 'is_admin', 'is_active','is_forget','is_webservice','webservice_from_who','webservice_origin','webservice_discount_code','webservice_campaign_name','webservice_total_request_limit','webservice_user_request_limit','webservice_question_price'),
         }),
     )
     search_fields = ('mobile_number', 'full_name')
--- accounts/models.py
+++ accounts/models.py
@@ -30,6 +30,7 @@ class CustomUser(AbstractBaseUser):
     webservice_campaign_name = models.CharField(max_length=255, null=True, blank=True)
     webservice_total_request_limit = models.IntegerField(default=10000)
     webservice_user_request_limit = models.IntegerField(default=30)
+    webservice_question_price = models.IntegerField(default=30000)
     balance = models.IntegerField(default=3)  #اعتبار اولیه با میزان پیش فرض 3 پرسش
 
     
